59cab6eef99a16f4fdf73970da2e56981ac14a3f
Carlos Ricardo Ortiz Sordo <ricardosordo82@gmail.com>
2020-06-27 14:19:39 -0500

    Understanding the props

---
+++ test-js/src/PrimeraApp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import '../src/index.css'
+
+const PrimeraApp = ({ saludo , subtitulo }) => {
+
+    return(
+        <>
+        <h1> { saludo } </h1>
+        <p> { subtitulo }</p>
+        </>
+    );
+}
+
+PrimeraApp.propTypes = {
+    saludo : PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtítulo'
+}
+
+
+export default PrimeraApp;
